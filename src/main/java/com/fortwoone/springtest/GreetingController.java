086d5263c436d92fbd4808190d28404bf3f04c2e
fortwoone <denauxarthur@gmail.com>
2025-09-29 11:12:59 +0200

    Added database access with simple scheme from Spring guide with MySQL

--- src/main/java/com/fortwoone/springtest/GreetingController.java
+++ src/main/java/com/fortwoone/springtest/GreetingController.java
@@ -3,7 +3,6 @@ package com.fortwoone.springtest;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
---
+++ src/main/java/com/fortwoone/springtest/UserRepository.java
@@ -0,0 +1,12 @@
+package com.fortwoone.springtest;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fortwoone.springtest.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
